283cdf1408711bc0a5c0a01d48b048ab978b2c5d
Denis <denis5396@gmail.com>
2020-08-08 13:58:35 +0200

    git commit

--- usluge.js
+++ usluge.js
@@ -48,6 +48,235 @@ window.addEventListener('load', (event) => {
       }
     }, 3000);
   }
+  const tagParent = document.querySelector('#tagcnt');
+  tagParent.addEventListener('click', clickTag);
+  function clickTag(e) {
+    console.log(e.target.textContent);
+    if (
+      e.target.className === 'tagcustom' ||
+      e.target.parentElement.className === 'tagcustom'
+    ) {
+      srchBox.value = e.target.textContent;
+      srchBoxInput = srchBox.value.toLowerCase();
+      srchCompare(srchBoxInput);
+    }
+  }
+
+  // searchbox
+  const rasvjetaS = document.querySelector('#rasvjeta');
+  const jakastrS = document.querySelector('#jakastr');
+  const slabastrS = document.querySelector('#slabastr');
+  const knxS = document.querySelector('#knx');
+  const dijagnozaS = document.querySelector('#dijagnoza');
+  const sigurnostS = document.querySelector('#sigurnost');
+  const planiranjeS = document.querySelector('#planiranje');
+  const uslugeArr = [
+    'rasvjeta',
+    'jaka jake',
+    'slaba slabe',
+    'knx ets pametna kuća smart home',
+    'dijagnoza i popravka',
+    'sigurnost video nadzor alarm kamere kamera',
+    'planiranje projektovanje planiranja projekt plan projekat',
+  ];
+  const srchBox = document.querySelector('#sbox');
+  let srchBoxInput;
+  srchBox.addEventListener('input', (e) => {
+    srchBoxInput = e.target.value.toLowerCase();
+    // console.log(srchBoxInput);
+    srchCompare(srchBoxInput);
+  });
+
+  right.addEventListener('click', () => {
+    // console.log(count);
+    if (clickable) {
+      clickable = false;
+      count++;
+
+      sliderux.style.transition = 'transform 0.5s ease';
+
+      changeSlide();
+      setTimeout(() => {
+        clickable = false;
+        if (count === sld.length - 1) {
+          sliderux.style.transition = 'none';
+        }
+        identify();
+      }, 1000);
+    }
+  });
+  left.addEventListener('click', () => {
+    // console.log(count);
+    if (clickable) {
+      clickable = false;
+      count--;
+
+      sliderux.style.transition = 'transform 0.5s ease';
+
+      changeSlide();
+      setTimeout(() => {
+        clickable = false;
+        if (count === 0) {
+          sliderux.style.transition = 'none';
+        }
+        identify();
+      }, 1000);
+    }
+  });
+
+  function changeSlide() {
+    sliderux.style.transform = `translateX(-${100 * count}vw)`;
+  }
+  // console.log(sld);
+  function identify() {
+    if (count === 0) {
+      count = sld.length - 2;
+
+      sliderux.style.transform = `translateX(-${100 * count}vw)`;
+    } else if (count === sld.length - 1) {
+      count = 1;
+      sliderux.style.transform = `translateX(-${100 * count}vw)`;
+    }
+    clickable = true;
+  }
+
+  // function scrollOnClick(elemBtn, elemName) {
+  //   // const totopvar = elemName.getBoundingClientRect().top + window.scrollY;
+
+  //   // console.log(totopvar);
+  //   // elemBtn.addEventListener('click', () => {
+  //   //   window.scrollTo({
+  //   //     top: totopvar,
+  //   //     behavior: 'smooth',
+  //   //   });
+  //   // });
+  //   elemBtn.addEventListener('click', () => {
+  //     scrollToItem(elemName);
+  //   });
+  // }
+  // function scrollToItem(item) {
+  //   var diff = (item.offsetTop - window.scrollY) / 8;
+  //   if (Math.abs(diff) > 1) {
+  //     window.scrollTo(0, window.scrollY + diff);
+  //     clearTimeout(window._TO);
+  //     window._TO = setTimeout(scrollToItem, 10, item);
+  //   } else {
+  //     window.scrollTo(0, item.offsetTop);
+  //   }
+  // }
+  function srchCompare(inputVal) {
+    for (let i = 0; i < newarrx.length; i++) {
+      for (let j = 0; j < newarrx[i].length; j++) {
+        if (inputVal.indexOf(newarrx[i][j]) !== -1) {
+          findMatch(newarrx[i][j]);
+        } else if (inputVal.length === 0) {
+          rasvjetaS.style.display = 'block';
+          knxS.style.display = 'block';
+          dijagnozaS.style.display = 'block';
+          planiranjeS.style.display = 'block';
+          slabastrS.style.display = 'block';
+          jakastrS.style.display = 'block';
+          sigurnostS.style.display = 'block';
+        }
+      }
+    }
+  }
+  let newarrx = [];
+  uslugeArr.forEach((item, idx) => {
+    newarrx[idx] = item.split(' ');
+  });
+
+  // console.log(newarrx);
+  function findMatch(findMatchVal) {
+    switch (findMatchVal) {
+      case 'rasvjeta':
+        rasvjetaS.style.display = 'block';
+        knxS.style.display = 'none';
+        dijagnozaS.style.display = 'none';
+        planiranjeS.style.display = 'none';
+        slabastrS.style.display = 'none';
+        jakastrS.style.display = 'none';
+        sigurnostS.style.display = 'none';
+        break;
+      case 'jaka':
+      case 'jake':
+        jakastrS.style.display = 'block';
+        knxS.style.display = 'none';
+        dijagnozaS.style.display = 'none';
+        planiranjeS.style.display = 'none';
+        slabastrS.style.display = 'none';
+        rasvjetaS.style.display = 'none';
+        sigurnostS.style.display = 'none';
+        break;
+      case 'slaba':
+      case 'slabe':
+        slabastrS.style.display = 'block';
+        slabastrS.style.marginTop = '10rem';
+        knxS.style.display = 'none';
+        dijagnozaS.style.display = 'none';
+        planiranjeS.style.display = 'none';
+        rasvjetaS.style.display = 'none';
+        jakastrS.style.display = 'none';
+        sigurnostS.style.display = 'none';
+        break;
+      case 'knx':
+      case 'pametna':
+      case 'kuća':
+      case 'ets':
+      case 'smart':
+      case 'home':
+        knxS.style.display = 'block';
+        rasvjetaS.style.display = 'none';
+        dijagnozaS.style.display = 'none';
+        planiranjeS.style.display = 'none';
+        rasvjetaS.style.display = 'none';
+        jakastrS.style.display = 'none';
+        slabastrS.style.display = 'none';
+        sigurnostS.style.display = 'none';
+        break;
+      case 'dijagnoza':
+      case 'popravka':
+        slabastrS.style.display = 'none';
+        knxS.style.display = 'none';
+        dijagnozaS.style.display = 'block';
+        planiranjeS.style.display = 'none';
+        rasvjetaS.style.display = 'none';
+        jakastrS.style.display = 'none';
+        slabastrS.style.display = 'none';
+        sigurnostS.style.display = 'none';
+        break;
+      case 'sigurnost':
+      case 'kamera':
+      case 'kamere':
+      case 'video':
+      case 'nadzor':
+      case 'alarm':
+        slabastrS.style.display = 'none';
+        knxS.style.display = 'none';
+        dijagnozaS.style.display = 'none';
+        planiranjeS.style.display = 'none';
+        rasvjetaS.style.display = 'none';
+        jakastrS.style.display = 'none';
+        slabastrS.style.display = 'none';
+        sigurnostS.style.display = 'block';
+        break;
+      case 'planiranje':
+      case 'projektovanje':
+      case 'planiranja':
+      case 'projekt':
+      case 'plan':
+      case 'projekat':
+        slabastrS.style.display = 'none';
+        knxS.style.display = 'none';
+        dijagnozaS.style.display = 'none';
+        planiranjeS.style.display = 'block';
+        rasvjetaS.style.display = 'none';
+        jakastrS.style.display = 'none';
+        slabastrS.style.display = 'none';
+        sigurnostS.style.display = 'none';
+        break;
+    }
+  }
 });
 // Auto slider
 
@@ -77,233 +306,3 @@ window.addEventListener('load', (event) => {
 // scrollOnClick(btnPlan, planSection);
 
 // clicktageventlist
-
-const tagParent = document.querySelector('#tagcnt');
-tagParent.addEventListener('click', clickTag);
-function clickTag(e) {
-  console.log(e.target.textContent);
-  if (
-    e.target.className === 'tagcustom' ||
-    e.target.parentElement.className === 'tagcustom'
-  ) {
-    srchBox.value = e.target.textContent;
-    srchBoxInput = srchBox.value.toLowerCase();
-    srchCompare(srchBoxInput);
-  }
-}
-
-// searchbox
-const rasvjetaS = document.querySelector('#rasvjeta');
-const jakastrS = document.querySelector('#jakastr');
-const slabastrS = document.querySelector('#slabastr');
-const knxS = document.querySelector('#knx');
-const dijagnozaS = document.querySelector('#dijagnoza');
-const sigurnostS = document.querySelector('#sigurnost');
-const planiranjeS = document.querySelector('#planiranje');
-const uslugeArr = [
-  'rasvjeta',
-  'jaka jake',
-  'slaba slabe',
-  'knx ets pametna kuća smart home',
-  'dijagnoza i popravka',
-  'sigurnost video nadzor alarm kamere kamera',
-  'planiranje projektovanje planiranja projekt plan projekat',
-];
-const srchBox = document.querySelector('#sbox');
-let srchBoxInput;
-srchBox.addEventListener('input', (e) => {
-  srchBoxInput = e.target.value.toLowerCase();
-  // console.log(srchBoxInput);
-  srchCompare(srchBoxInput);
-});
-
-right.addEventListener('click', () => {
-  // console.log(count);
-  if (clickable) {
-    clickable = false;
-    count++;
-
-    sliderux.style.transition = 'transform 0.5s ease';
-
-    changeSlide();
-    setTimeout(() => {
-      clickable = false;
-      if (count === sld.length - 1) {
-        sliderux.style.transition = 'none';
-      }
-      identify();
-    }, 1000);
-  }
-});
-left.addEventListener('click', () => {
-  // console.log(count);
-  if (clickable) {
-    clickable = false;
-    count--;
-
-    sliderux.style.transition = 'transform 0.5s ease';
-
-    changeSlide();
-    setTimeout(() => {
-      clickable = false;
-      if (count === 0) {
-        sliderux.style.transition = 'none';
-      }
-      identify();
-    }, 1000);
-  }
-});
-
-function changeSlide() {
-  sliderux.style.transform = `translateX(-${100 * count}vw)`;
-}
-// console.log(sld);
-function identify() {
-  if (count === 0) {
-    count = sld.length - 2;
-
-    sliderux.style.transform = `translateX(-${100 * count}vw)`;
-  } else if (count === sld.length - 1) {
-    count = 1;
-    sliderux.style.transform = `translateX(-${100 * count}vw)`;
-  }
-  clickable = true;
-}
-
-// function scrollOnClick(elemBtn, elemName) {
-//   // const totopvar = elemName.getBoundingClientRect().top + window.scrollY;
-
-//   // console.log(totopvar);
-//   // elemBtn.addEventListener('click', () => {
-//   //   window.scrollTo({
-//   //     top: totopvar,
-//   //     behavior: 'smooth',
-//   //   });
-//   // });
-//   elemBtn.addEventListener('click', () => {
-//     scrollToItem(elemName);
-//   });
-// }
-// function scrollToItem(item) {
-//   var diff = (item.offsetTop - window.scrollY) / 8;
-//   if (Math.abs(diff) > 1) {
-//     window.scrollTo(0, window.scrollY + diff);
-//     clearTimeout(window._TO);
-//     window._TO = setTimeout(scrollToItem, 10, item);
-//   } else {
-//     window.scrollTo(0, item.offsetTop);
-//   }
-// }
-function srchCompare(inputVal) {
-  for (let i = 0; i < newarrx.length; i++) {
-    for (let j = 0; j < newarrx[i].length; j++) {
-      if (inputVal.indexOf(newarrx[i][j]) !== -1) {
-        findMatch(newarrx[i][j]);
-      } else if (inputVal.length === 0) {
-        rasvjetaS.style.display = 'block';
-        knxS.style.display = 'block';
-        dijagnozaS.style.display = 'block';
-        planiranjeS.style.display = 'block';
-        slabastrS.style.display = 'block';
-        jakastrS.style.display = 'block';
-        sigurnostS.style.display = 'block';
-      }
-    }
-  }
-}
-let newarrx = [];
-uslugeArr.forEach((item, idx) => {
-  newarrx[idx] = item.split(' ');
-});
-
-// console.log(newarrx);
-function findMatch(findMatchVal) {
-  switch (findMatchVal) {
-    case 'rasvjeta':
-      rasvjetaS.style.display = 'block';
-      knxS.style.display = 'none';
-      dijagnozaS.style.display = 'none';
-      planiranjeS.style.display = 'none';
-      slabastrS.style.display = 'none';
-      jakastrS.style.display = 'none';
-      sigurnostS.style.display = 'none';
-      break;
-    case 'jaka':
-    case 'jake':
-      jakastrS.style.display = 'block';
-      knxS.style.display = 'none';
-      dijagnozaS.style.display = 'none';
-      planiranjeS.style.display = 'none';
-      slabastrS.style.display = 'none';
-      rasvjetaS.style.display = 'none';
-      sigurnostS.style.display = 'none';
-      break;
-    case 'slaba':
-    case 'slabe':
-      slabastrS.style.display = 'block';
-      slabastrS.style.marginTop = '10rem';
-      knxS.style.display = 'none';
-      dijagnozaS.style.display = 'none';
-      planiranjeS.style.display = 'none';
-      rasvjetaS.style.display = 'none';
-      jakastrS.style.display = 'none';
-      sigurnostS.style.display = 'none';
-      break;
-    case 'knx':
-    case 'pametna':
-    case 'kuća':
-    case 'ets':
-    case 'smart':
-    case 'home':
-      knxS.style.display = 'block';
-      rasvjetaS.style.display = 'none';
-      dijagnozaS.style.display = 'none';
-      planiranjeS.style.display = 'none';
-      rasvjetaS.style.display = 'none';
-      jakastrS.style.display = 'none';
-      slabastrS.style.display = 'none';
-      sigurnostS.style.display = 'none';
-      break;
-    case 'dijagnoza':
-    case 'popravka':
-      slabastrS.style.display = 'none';
-      knxS.style.display = 'none';
-      dijagnozaS.style.display = 'block';
-      planiranjeS.style.display = 'none';
-      rasvjetaS.style.display = 'none';
-      jakastrS.style.display = 'none';
-      slabastrS.style.display = 'none';
-      sigurnostS.style.display = 'none';
-      break;
-    case 'sigurnost':
-    case 'kamera':
-    case 'kamere':
-    case 'video':
-    case 'nadzor':
-    case 'alarm':
-      slabastrS.style.display = 'none';
-      knxS.style.display = 'none';
-      dijagnozaS.style.display = 'none';
-      planiranjeS.style.display = 'none';
-      rasvjetaS.style.display = 'none';
-      jakastrS.style.display = 'none';
-      slabastrS.style.display = 'none';
-      sigurnostS.style.display = 'block';
-      break;
-    case 'planiranje':
-    case 'projektovanje':
-    case 'planiranja':
-    case 'projekt':
-    case 'plan':
-    case 'projekat':
-      slabastrS.style.display = 'none';
-      knxS.style.display = 'none';
-      dijagnozaS.style.display = 'none';
-      planiranjeS.style.display = 'block';
-      rasvjetaS.style.display = 'none';
-      jakastrS.style.display = 'none';
-      slabastrS.style.display = 'none';
-      sigurnostS.style.display = 'none';
-      break;
-  }
-}
